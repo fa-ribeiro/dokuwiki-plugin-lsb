15693795a623b25b907f1cd8c13a2467e6e76cbd
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-09-19 21:50:34 +0200

    translation update

---
+++ lang/ru/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['networks']              = 'Список избранных социальных кнопок, разделённый пробелами и&nbsp;в&nbsp;порядке их&nbsp;отображения. Допустимые имена: twitter facebook googleplus linkedin pinterest tumblr reddit taringa stumbleupon telegram hackernews xing vk email';
+$lang['display']               = 'Варианты отображения: показывать социальные кнопки с&nbsp;иконкой и&nbsp;названием (name), только иконку (icon) или название на&nbsp;цветном фоне (color)';
+$lang['target']                = 'target для ссылок: как открыть страницу социальной сети';
+$lang['twitter_user']          = 'Имя пользователя в&nbsp;Twitter автора контента (без&nbsp;@)';
